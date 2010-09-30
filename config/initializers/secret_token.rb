4ffadd4b2f7bb7682d3ca7e2ecb9e62563f57a34
docp <paul_harmsen@web.de>
2010-09-30 13:37:18 +0200

    Second start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trackdb::Application.config.secret_token = '9ce1bbaacf39da32a18b8cbbb1908ab3429607e5f169af41580107ab18f840a810ea6f11f6c9bf93efa300a4f1444c428ab242d826295dc2674906e46c52ae16'
