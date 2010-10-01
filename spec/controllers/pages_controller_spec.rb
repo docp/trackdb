02c7dd715c01ef1599f75b35b38abbcba23bc52b
docp <paul_harmsen@web.de>
2010-10-01 12:22:02 +0200

    Tests for page Titles implemented

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,17 +1,18 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "should load successful" do
       get 'home'
       response.should be_success
     end
-#    it "should have the right title" do
-#      get 'home'
-#      response.should have_selector("title",
-#                                    :content => "Track database | Home")
-#    end
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title",
+                                    :content => "Track Database | Home")
+    end
   end
 
   describe "GET 'contact'" do
@@ -20,12 +21,12 @@ describe PagesController do
       response.should be_success
     end
 
-#    it "should have the right title" do
-#      get 'contact'
-#      response.should have_selector("title",
-#                      :content => "Track database | Contact")
-#    end
-      end
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title",
+                                      :content => "Track Database | Contact")
+    end
+  end
 
   describe "GET 'about'" do
     it "should load successful" do
@@ -33,15 +34,15 @@ describe PagesController do
       response.should be_success
     end
 
-#    it "should have the right title" do
-#      get 'about'
-#      response.should have_selector("title",
-#                      :content => "Track database | About")
-#    end
-#    it "should contain written by paul" do
-#      get 'about'
-#      response.should have_selector("body", :content => "written by paul")
-#    end
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title",
+                      :content => "Track Database | About")
+    end
+    it "should contain written by paul" do
+      get 'about'
+      response.should have_selector("body", :content => "written by paul")
+    end
   end
 
 
