c8d6a53a9e08bfe06179671183f73ab2ee4f9e5d
docp <paul_harmsen@web.de>
2010-11-06 20:26:22 +0100

    User signup ready

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,18 @@ class UsersController < ApplicationController
 
   def new
     @title = "Sign up"
+    @user = User.new
   end
 
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to @user
+      flash[:success] = "Welcome to the Track Database"
+    else
+      @title = "Sign up"
+      render :new
+    end
+  end
 end
 
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -48,5 +48,61 @@ describe UsersController do
                                     :content => @base_title + " | Sign up")
     end
   end
+
+  describe "POST 'create'" do
+    describe "failure" do
+      before(:each) do
+        @attr = { :name => "", :email => "", :password => "",
+                  :password_confirmation => ""}
+      end
+      it "should not create a user" do
+        lambda do
+          post :create, :user => @attr
+        end.should_not change(User, :count)
+      end
+
+      it "should have the right title" do
+        post :create, :user => @attr
+        response.should have_selector("title",
+                                    :content => @base_title + " | Sign up")
+      end
+
+      it "should render the 'new' page" do
+        post :create, :user => @attr
+        response.should render_template('new')
+      end
+    end
+
+    describe "success" do
+      before(:each) do
+		  @attr = { :name => "Max Mustermann",
+		            :email => "max@mustermann.de",
+		            :password => "Geheim_Geheim",
+		            :password_confirmation => "Geheim_Geheim" }
+	    end
+
+      it "should create a user" do
+        lambda do
+          post :create, :user => @attr
+        end.should change(User, :count).by(1)
+      end
+
+      it "should redirect to the user show page" do
+        post :create, :user => @attr
+        response.should redirect_to(user_path(assigns(:user)))
+      end
+
+      it "should generate a welcome message" do
+        post :create, :user => @attr
+        flash[:success].should =~ /welcome to the track database/i
+      end
+#      No Way found to test the ridirect with rspec, should be possible with webrat
+#      it "should display a welcome message" do
+#        post :create, :user => @attr
+#        response.should have_selector(:div,
+#                                  :content => "Welcome to the " + @base_title)
+#      end
+    end
+  end
 end
 
